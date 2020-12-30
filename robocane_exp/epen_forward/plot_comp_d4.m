ec78bebc8210615015c7044e07988ade59ed7a2d
He Zhang <hxzhang1@ualr.edu>
2020-12-29 23:12:15 -0500

    add more results

--- robocane_exp/epen_forward/plot_comp_d4.m
+++ robocane_exp/epen_forward/plot_comp_d4.m
@@ -16,15 +16,16 @@ tr4 = dlmread('./comp_d4/d4_dvio_w_ini.csv');
 
 plot_xyz(tr1(:,2), tr1(:,3), tr1(:,4), 'r-');
 hold on;
-plot_xyz(tr2(:,2), tr2(:,3), tr2(:,4), 'c-');
-hold on;
+% plot_xyz(tr2(:,2), tr2(:,3), tr2(:,4), 'c-');
+% hold on;
 plot_xyz(tr3(:,2), tr3(:,3), tr3(:,4), 'g-'); 
 hold on;
 plot_xyz(tr4(:,2), tr4(:,3), tr4(:,4), 'b-'); 
 hold on;
 grid on
 plot3(0, 20.0, 0, 'k*');
-legend('VINS-Mono', 'VINS-RGBD', 'DVIO', 'DVIO w Initial'); 
+% legend('VINS-Mono', 'VINS-RGBD', 'DVIO', 'DVIO w Initial'); 
+legend('VINS-Mono', 'DVIO', 'DVIO w Initial', 'True Endpoint'); 
 
 
 
